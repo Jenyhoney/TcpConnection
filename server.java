e74aee9623144ee378a7307da833a2778824f175
Jenyhoney <36188597+Jenyhoney@users.noreply.github.com>
2018-04-04 05:18:21 +0000

    Update server.java

--- server.java
+++ server.java
@@ -1,6 +1,7 @@
     import java.net.*;  
     import java.io.*;  
-    class server{  
+    class server
+    {  
     public static void main(String args[])throws Exception{  
     ServerSocket ss=new ServerSocket(3333);  
     Socket s=ss.accept();  
@@ -19,4 +20,5 @@
     din.close();  
     s.close();  
     ss.close();  
-    }}  
+    }
+    }  
