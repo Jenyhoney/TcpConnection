c04109d470d6546a8bef90a91845376feb39f48b
treesarose <36188557+treesarose@users.noreply.github.com>
2018-04-04 10:52:27 +0530

    Update server.java

--- server.java
+++ server.java
@@ -1,14 +1,12 @@
     import java.net.*;  
     import java.io.*;  
-    class server
-    {  
+    class server{  
     public static void main(String args[])throws Exception{  
     ServerSocket ss=new ServerSocket(3333);  
     Socket s=ss.accept();  
     DataInputStream din=new DataInputStream(s.getInputStream());  
     DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-      
     String str="",str2="";  
     while(!str.equals("stop")){  
     str=din.readUTF();  
