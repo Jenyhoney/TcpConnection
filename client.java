674e9cfe84b808e9a8b84f0237fe7cd6480a758e
treesarose <36188557+treesarose@users.noreply.github.com>
2018-04-04 10:50:09 +0530

    Update client.java

--- client.java
+++ client.java
@@ -6,7 +6,6 @@ Socket s=new Socket("localhost",3333);
 DataInputStream din=new DataInputStream(s.getInputStream());  
 DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-  
 String str="",str2="";  
 while(!str.equals("stop")){  
 str=br.readLine();  
@@ -15,7 +14,6 @@ dout.flush();
 str2=din.readUTF();  
 System.out.println("Server says: "+str2);  
 }  
-  
 dout.close();  
 s.close();
 }
